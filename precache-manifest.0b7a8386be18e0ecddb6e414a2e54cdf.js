c54446a10fd87b33be6f4e78a2e2db4479302752
sinamoraddar <sinamoraddar@gmail.com>
2019-02-05 19:59:14 +0000

    Updates

---
+++ precache-manifest.0b7a8386be18e0ecddb6e414a2e54cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RoboFriendApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d223650f911b04c6c23",
+    "url": "/RoboFriendApp/static/js/runtime~main.2d223650.js"
+  },
+  {
+    "revision": "b25757bdacb0e2576401",
+    "url": "/RoboFriendApp/static/js/main.b25757bd.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/RoboFriendApp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b25757bdacb0e2576401",
+    "url": "/RoboFriendApp/static/css/main.f003cda2.chunk.css"
+  },
+  {
+    "revision": "b7c02390194d960bfb618c84339f4441",
+    "url": "/RoboFriendApp/index.html"
+  }
+];
